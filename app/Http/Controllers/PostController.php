cef9d8dbd4e56f6007c872bc6454a251422cf9e8
muru78gp01 <s185004@matsu.shimane-u.ac.jp>
2023-05-05 18:29:26 +0900

    1.web.phpに投稿詳細へのURLのルーティングを追記。 2.PostControllerに'posts/show'へのルーティングで呼び出されるshow関数を追加。 3.index.blade.phpにて投稿一覧のタイトルをリンクに変更。押下すると詳細画面に遷移するように。 4.投稿詳細画面のView。show.blade.phpを追加。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,4 +20,16 @@ class PostController extends Controller
         //blade内で使う変数'posts''と設定。'posts'の中身にupdated_atで降順に並べたあと、
         //10件の件数制限をかけて取得するgetPagenateBylimit()を使い、インスタンス化した$postを代入。
     }
+    
+    /**
+     * 特定IDのpostを表示する
+     * 
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reponse post view
+     */
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 }
