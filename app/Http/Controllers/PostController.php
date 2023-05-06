951e995d12c55b9ffcb5a0e0f34346701a256afe
muru78gp01 <s185004@matsu.shimane-u.ac.jp>
2023-05-07 05:12:53 +0900

    1.web.phpに保存ボタンが押下された時のルーティングを追加。 2.PostControllerにpostインスタンスに含まれるtitleとbodyを取得してINSERT文SQLを作成&実行する関数storeを実装。 3.Post.phpにfillを実行するためのプロパティ設定を追記。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,6 +26,49 @@ class PostController extends Controller
         return view('posts/create');
     }
     
+    public function store(Request $request, Post $post)
+    /**Request $request
+     * ユーザからのリクエストが含まれるデータを扱う場合、Requestインスタンスを利用する。
+     * Post $post
+     * この関数ではユーザの入力データをDBのpostsテーブルにアクセスし保存する必要があるため、
+     * 空のPostインスタンスを利用する。
+     */
+    {
+        /**リクエストの中身
+         * $request['post']を利用すると、postをキーに持つリクエストパラメータを取得できる。
+         * $requestのキーは、HTMLのFormタグ内で定義した各入力項目のname属性と一致する。
+         * 今回は入れ子構造になっているため、このような取得をすると
+         * $inputは[ 'title' => 'タイトル', 'body' => '本文']のような配列型式となる。
+         */
+        
+        /**保存処理
+         * ・インスタンスのプロパティを上書きする
+         * 　$post->fill($input)とすることで、先ほどまでからだったPostインスタンスのプロパティを、
+         * 　受け取ったキーごとに上書きができる。
+         * 　具体的には、$post->titleはタイトル、$post->bodyは本文という値になる。
+         * 　ただし、fillを実行するとき、PostModel側でfillableというプロパティにfillが可能なプロパティを指定しておく必要がある。
+         * 
+         * ・保存処理を実行する
+         * 　プロパティを上書きしたインスタンスをsaveすることで、フレームワーク内部でMySQLへのINSERT文が実行され、
+         * 　DBへデータが追加される。
+         * 　通常、SQLを作成し実行しなければデータの追加は行えない。
+         * 　LaravelではEloquentというORマッパーが用意されており、モデルクラスがそれにあたる。
+         * 　fill関数やsave関数はこのモデルクラスに含まれる。
+         * 　これを実行することで、内部的にSQLの作成&実行が行われるので、
+         * 　頑張ってSQLをソース内に書かなくても、データの取得や追加などができる。
+         */
+        
+        /**リダイレクト
+         * 保存処理が終了すると、/posts/1など、今回保存したpostのIDを含んだRLにリダイレクトする。
+         * $post->save()が完了した段階で、PostインスタンスにはIDが採番され、
+         * プロパティとしても保持しておりアクセス可能。
+         * そのため、return redirect('posts/' . $post->id);と記載できる。
+         */
+        $input = $request['post'];//$input = [ 'title' => 'タイトル', 'body' => '本文']
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
     /**
      * 特定IDのpostを表示する
      * 
